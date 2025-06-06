cbde1a1f5eb524d9242251f61831ba12b0ec8dfa
Ardyyy <86863270+ArdyBotzz@users.noreply.github.com>
2025-06-06 12:06:25 +0800

    Update NatHub.lua

--- NatHub.lua
+++ NatHub.lua
@@ -3,33 +3,39 @@ local supportedGames = {
     [7018190066] = "https://api.luarmor.net/files/v3/loaders/d3a76114c1ea182127b88170b6043d11.lua", -- Dead Rails
     [5750914919] = "https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/fisch.lua", -- Fisch
     [6325068386] = "https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/blr.lua", -- BLR
-	[4777817887] = "https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/bladeball.lua", -- Bladeball
+    [4777817887] = "https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/bladeball.lua", -- Bladeball
     [7095682825] = "https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/beaks.lua", -- Beaks
-	[994732206] = "https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/bf.lua" -- Blox Fruit
+    [994732206] = "https://raw.githubusercontent.com/ArdyBotzz/NatHub/refs/heads/master/bf.lua" -- Blox Fruit
 }
 local gameNames = {
     [5750914919] = "Fisch", -- Fisch
     [7018190066] = "Dead Rails", -- Dead Rails
     [6325068386] = "Blue Lock Rivals", -- BLR
-	[4777817887] = "Blade Ball", -- Blade Ball
-	[7436755782] = "GaG", -- GaG
+    [4777817887] = "Blade Ball", -- Blade Ball
+    [7436755782] = "GaG", -- GaG
     [7095682825] = "Beaks", -- Beaks
-	[994732206] = "Blox Fruit"
+    [994732206] = "Blox Fruit"
 }
 local gameId = game.GameId
 if supportedGames[gameId] then
-    game.StarterGui:SetCore("SendNotification", {
-        Title = "NatHub Loaded!";
-        Text = gameNames[gameId] .. " Script Loaded!";
-        Icon =  "rbxassetid://99764942615873";
-        Duration = 5;
-    })
+    game.StarterGui:SetCore(
+        "SendNotification",
+        {
+            Title = "NatHub Loaded!",
+            Text = gameNames[gameId] .. " Script Loaded!",
+            Icon = "rbxassetid://99764942615873",
+            Duration = 5
+        }
+    )
     loadstring(game:HttpGet(supportedGames[gameId]))()
 else
-    game.StarterGui:SetCore("SendNotification", {
-        Title = "NatHub Error!";
-        Text = "You Execute Non Supported Game";
-        Icon =  "rbxassetid://99764942615873";
-        Duration = 5;
-    })
+    game.StarterGui:SetCore(
+        "SendNotification",
+        {
+            Title = "NatHub Error!",
+            Text = "You Execute Non Supported Game",
+            Icon = "rbxassetid://99764942615873",
+            Duration = 5
+        }
+    )
 end
